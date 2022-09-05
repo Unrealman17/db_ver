b980e3a6f9d60ec3ed0edf323d2416c3f6db41a0
EvVa <EvVa>
2022-09-05 09:55:30 +0300

    ver fix

--- update/up_script.sql
+++ update/up_script.sql
@@ -1,7 +1,6 @@
--- version = 2
+-- version = 1
 /*
     you can use "\i 'function/reclada_object.get_schema.sql'"
     to run text script of functions
 */
 
-create table public.test (id int, val text);
