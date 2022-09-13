eb3c3687db036a59bb281ff2adc77ca5b9899114
EvVa <EvVa>
2022-09-13 09:28:25 +0300

    bug fix

--- update/update_db.py
+++ update/update_db.py
@@ -63,13 +63,13 @@ class DBHelper:
         checkout(self.branch_db)
 
         res = os.popen(
-            f'git log --pretty=format:"%H" --first-parent 8d9ebc202cdc00e0603b1fa0c806cf1d06b111a4..').readlines()
+            f'git log --pretty=format:"%H" --first-parent b760cee39031f06e75a298e666df0c89efc8b0ef..').readlines()
         for i in range(len(res)):
             res[i] = res[i].strip()
         res.reverse()
         if need_comment:
             res2 = os.popen(
-                'git log --pretty=format:"%B" --first-parent 8d9ebc202cdc00e0603b1fa0c806cf1d06b111a4..').readlines()
+                'git log --pretty=format:"%B" --first-parent b760cee39031f06e75a298e666df0c89efc8b0ef..').readlines()
             while('\n' in res2):
                 res2.remove('\n')
             for i in range(len(res2)):
